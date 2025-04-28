da01ba133bb6a71e8d445cdc562742912192a3cc
Miguel Martins <miguelmartins@Miguels-MacBook-Pro.local>
2025-04-28 21:53:39 +0100

    Add some opts from kickstart.nvim
    Some useful opts from kicstram nvim for imporved qol.

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -5,6 +5,9 @@ vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true })
 vim.api.nvim_set_keymap("n", "n", "nzzzv", { noremap = true })
 vim.api.nvim_set_keymap("n", "N", "Nzzzv", { noremap = true })
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- tmux integration
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
@@ -14,6 +17,11 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 -- options 
 vim.opt.expandtab = true
 vim.opt.guicursor = "n-v-c:block-Cursor/blinkon1"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.inccommand = 'split'
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 8  -- never have less than 8 lines at eof
