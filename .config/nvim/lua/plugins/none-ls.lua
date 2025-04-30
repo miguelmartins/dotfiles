c3062476e453db365a641d2e59a460ae3caa348d
Miguel Martins <miguelmartins@Miguels-MacBook-Pro.local>
2025-04-30 12:50:30 +0100

    Fix mason-null-ls installation.

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,15 +1,33 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
+	{
+		"jay-babu/mason-null-ls.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"jose-elias-alvarez/null-ls.nvim",
+		},
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = { "jay-babu/mason-null-ls.nvim" },
+		config = function()
+			require("mason").setup()
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					-- Opt to list sources here, when available in mason.
+				},
+				automatic_installation = false,
+				handlers = {},
+			})
+			local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.prettier.with({ filetypes = { "html", "json", "yaml", "markdown" } }),
-                null_ls.builtins.formatting.shfmt.with({ args = { "-i", "4" } }),
-                null_ls.builtins.formatting.stylua,
-            },
-        })
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.prettier.with({ filetypes = { "html", "json", "yaml", "markdown" } }),
+					null_ls.builtins.formatting.shfmt.with({ args = { "-i", "4" } }),
+					null_ls.builtins.formatting.stylua,
+				},
+			})
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
 }
