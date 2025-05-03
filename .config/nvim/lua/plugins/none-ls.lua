99c2a5bfb12e334cc9e0d5c2da8bf9a4570812a7
Miguel Martins <miguelmartins@Miguels-MacBook-Pro.local>
2025-05-03 12:13:54 +0100

    Blink for cmp and conform for linting
    fixup

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,44 +1,44 @@
-return {
-	{
-		"jay-babu/mason-null-ls.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"jose-elias-alvarez/null-ls.nvim",
-		},
-	},
-	{
-		"nvimtools/none-ls.nvim",
-		dependencies = { "jay-babu/mason-null-ls.nvim" },
-		config = function()
-			require("mason").setup()
-			require("mason-null-ls").setup({
-				ensure_installed = {
-					-- Opt to list sources here, when available in mason.
-                   "lua_ls",
-                    "prettier", -- ts/js formatter
-                    "stylua", -- lua formatter
-                    "shfmt",
-                    "ruff",
-				},
-				automatic_installation = false,
-				handlers = {},
-			})
-			local null_ls = require("null-ls")
-
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.prettier.with({ filetypes = { "html", "json", "yaml", "markdown" } }),
-					null_ls.builtins.formatting.shfmt.with({ args = { "-i", "4" } }),
-					null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.diagnostics.ruff.with({
-          -- pass any extra args you like, e.g. max-line-length:
-          extra_args = {'--extend-select', 'I'},
-        }),
-        -- (Optional) Ruff as a formatter
-        null_ls.builtins.formatting.ruff,
-				},
-			})
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-		end,
-	},
-}
+return {}
+---	{
+---		"jay-babu/mason-null-ls.nvim",
+---		dependencies = {
+---			"williamboman/mason.nvim",
+---			"jose-elias-alvarez/null-ls.nvim",
+---		},
+---	},
+---	{
+---		"nvimtools/none-ls.nvim",
+---		dependencies = { "jay-babu/mason-null-ls.nvim" },
+---		config = function()
+---			require("mason").setup()
+---			require("mason-null-ls").setup({
+---				ensure_installed = {
+---					-- Opt to list sources here, when available in mason.
+---                   "lua_ls",
+---                    "prettier", -- ts/js formatter
+---                    "stylua", -- lua formatter
+---                    "shfmt",
+---                    "ruff",
+---				},
+---				automatic_installation = false,
+---				handlers = {},
+---			})
+---			local null_ls = require("null-ls")
+---
+---			null_ls.setup({
+---				sources = {
+---					null_ls.builtins.formatting.prettier.with({ filetypes = { "html", "json", "yaml", "markdown" } }),
+---					null_ls.builtins.formatting.shfmt.with({ args = { "-i", "4" } }),
+---					null_ls.builtins.formatting.stylua,
+---                    null_ls.builtins.diagnostics.ruff.with({
+---          -- pass any extra args you like, e.g. max-line-length:
+---          extra_args = {'--extend-select', 'I'},
+---        }),
+---        -- (Optional) Ruff as a formatter
+---        null_ls.builtins.formatting.ruff,
+---				},
+---			})
+---			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+---		end,
+---	},
+---}
