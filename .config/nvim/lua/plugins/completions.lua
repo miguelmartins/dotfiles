885f6d7d8e6312de056a22bc53ad149861d9ca90
Miguel Martins <miguelmartins@Miguels-MacBook-Pro.local>
2025-05-04 18:29:05 +0100

    Reneable LuaSnip

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -38,40 +38,16 @@ return {
 			"folke/lazydev.nvim",
 			"micangl/cmp-vimtex",
 		},
+		lazy = true,
 		--- @module 'blink.cmp'
 		--- @type blink.cmp.Config
 		opts = {
 			keymap = {
-				-- 'default' (recommended) for mappings similar to built-in completions
-				--   <c-y> to accept ([y]es) the completion.
-				--    This will auto-import if your LSP supports it.
-				--    This will expand snippets if the LSP sent a snippet.
-				-- 'super-tab' for tab to accept
-				-- 'enter' for enter to accept
-				-- 'none' for no mappings
-				--
-				-- For an understanding of why the 'default' preset is recommended,
-				-- you will need to read `:help ins-completion`
-				--
-				-- No, but seriously. Please read `:help ins-completion`, it is really good!
-				--
-				-- All presets have the following mappings:
-				-- <tab>/<s-tab>: move to right/left of your snippet expansion
-				-- <c-space>: Open menu or open docs if already open
-				-- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-				-- <c-e>: Hide menu
-				-- <c-k>: Toggle signature help
-				--
-				-- See :h blink-cmp-config-keymap for defining your own keymap
 				preset = "default",
-
-				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 			},
 
 			appearance = {
-				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				-- Adjusts spacing to ensure icons are aligned
 				nerd_font_variant = "mono",
 			},
 
@@ -81,8 +57,9 @@ return {
 				documentation = { auto_show = false, auto_show_delay_ms = 500 },
 			},
 
+			snippets = { preset = "luasnip" },
 			sources = {
-				default = { "lsp", "path", "snippets", "lazydev" },
+				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
 				providers = {
 					lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
 					vimtex = {
@@ -92,8 +69,6 @@ return {
 					},
 				},
 			},
-
-			snippets = { preset = "luasnip" },
 
 			-- Blink.cmp includes an optional, recommended rust fuzzy matcher,
 			-- which automatically downloads a prebuilt binary when enabled.
@@ -107,6 +82,7 @@ return {
 			-- Shows a signature help window while you type arguments for a function
 			signature = { enabled = true },
 		},
+		opts_extend = { "sources.default" },
 	},
 }
 --  {
--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -2,7 +2,7 @@ return {
 	"L3MON4D3/LuaSnip",
 	version = "v2.*",
 	dependencies = {
-		--	"saadparwaiz1/cmp_luasnip",
+		"saadparwaiz1/cmp_luasnip",
 		"rafamadriz/friendly-snippets",
 	},
 	build = "make install_jsregexp",
@@ -18,23 +18,23 @@ return {
 		})
 		local ls = require("luasnip")
 		ls.setup({
-			--	update_events = { "TextChanged", "TextChangedI" },
-			--	enable_autosnippets = true,
+			update_events = { "TextChanged", "TextChangedI" },
+			enable_autosnippets = true,
 			-- store_selection_keys = "<Tab>",
 		})
-		vim.keymap.set({ "i" }, "<C-k>", function()
-			ls.expand()
-		end, { silent = true, desc = "expand autocomplete" })
-		vim.keymap.set({ "i", "s" }, "<C-j>", function()
-			ls.jump(1)
-		end, { silent = true, desc = "next autocomplete" })
-		vim.keymap.set({ "i", "s" }, "<C-L>", function()
-			ls.jump(-1)
-		end, { silent = true, desc = "previous autocomplete" })
-		vim.keymap.set({ "i", "s" }, "<C-E>", function()
-			if ls.choice_active() then
-				ls.change_choice(1)
-			end
-		end, { silent = true, desc = "select autocomplete" })
+		-- vim.keymap.set({ "i" }, "<C-k>", function()
+		-- 	ls.expand()
+		-- end, { silent = true, desc = "expand autocomplete" })
+		-- vim.keymap.set({ "i", "s" }, "<C-j>", function()
+		-- 	ls.jump(1)
+		-- end, { silent = true, desc = "next autocomplete" })
+		-- vim.keymap.set({ "i", "s" }, "<C-L>", function()
+		-- 	ls.jump(-1)
+		-- end, { silent = true, desc = "previous autocomplete" })
+		-- vim.keymap.set({ "i", "s" }, "<C-E>", function()
+		-- 	if ls.choice_active() then
+		-- 		ls.change_choice(1)
+		-- 	end
+		-- end, { silent = true, desc = "select autocomplete" })
 	end,
 }
